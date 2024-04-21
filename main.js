5604dc74907231cb2fcd7cc29dc03cf636f23165
Muhammad Umer <umer15325@gmail.com>
2024-04-21 08:48:09 -0700

    code uploaded

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// WORD COUNTER
+// npx wordcount_15325
+// Modules
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Code
+const counter = await inquirer.prompt([
+    {
+        name: "words",
+        message: chalk.yellow("Enter a phrase to count the words:") + " ",
+        type: "input"
+    }
+]);
+const wordCount = counter.words.trim().split(" ");
+console.log(chalk.greenBright(`Number of words in given phrase is ${wordCount.length}.`));
